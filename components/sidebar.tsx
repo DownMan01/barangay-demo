4badba3ea856ed71a9d1f2e7447de80d1b7ef1ab
J <98541904+DownMan01@users.noreply.github.com>
2025-02-27 00:27:49 +0800

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -34,7 +34,7 @@ export function Sidebar() {
       <div className="p-6">
         <div className="flex items-center gap-2 font-semibold">
           <Home className="h-6 w-6" />
-          <a> href="/favicon.png" BIMS</a>
+         <a href="/">BIMS</a>
         </div>
       </div>
       <nav className="flex-1 space-y-1 p-4">
